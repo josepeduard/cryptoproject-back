67a406b88541fd28f8e11954fd0866127c56eec3
josepeduard <j.e.garriga.obach@gmail.com>
2019-03-29 12:57:44 +0100

    wallet

--- models/wallet-fiat.js
+++ models/wallet-fiat.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const walletSchema = new Schema({
-  owner: onjectId,
-  cantidad: number,
+  owner: ObjectId,
+  quantity: Number,
   currency: String
 });
 
